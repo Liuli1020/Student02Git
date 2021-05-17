0e84ba4aab0eedde98be12aa9a0f090dd5f8ac32
Liuli1020 <2421594838@qq.com>
2021-05-17 09:46:49 +0800

    再次将student02Git文件提交

--- student02git/src/com/git/TestGitDemo01.java
+++ student02git/src/com/git/TestGitDemo01.java
@@ -3,6 +3,7 @@ package com.git;
 public class TestGitDemo01 {
     public static void main(String[] args) {
         System.out.println("这是TestGitDemo01");
-       System.out.println("修改了TestGitDemo01");
+     //  System.out.println("修改了TestGitDemo01");
+        System.out.println("再次修改");
     }
 }
